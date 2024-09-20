11c85b373d071dfd9e0a454a8f8a3a756757ef38
Dmitrii Malinovskii <you@example.com>
2024-09-20 23:16:31 +0200

    add user

--- feature/discussion/server/server_test.go
+++ feature/discussion/server/server_test.go
@@ -2,6 +2,8 @@ package server_test
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestServer(t *testing.T) {
@@ -10,11 +12,24 @@ func TestServer(t *testing.T) {
 	t.Run("authorization", func(t *testing.T) {
 		t.Parallel()
 
+		var repo userRepo
+
 		// Given there is a user Dima.
+
+		_, err := repo.GetUser("Dima")
+		require.NoError(t, err, "getting user Dima")
 
 		// When Dima authorises.
 
 		// Then he can do it.
 
 	})
+}
+
+type user struct{}
+
+type userRepo struct{}
+
+func (r *userRepo) GetUser(id string) (*user, error) {
+	return &user{}, nil
 }
