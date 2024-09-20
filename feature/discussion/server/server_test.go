69816987f6ebc7a75572855d540598747dfd2c29
Dmitrii Malinovskii <you@example.com>
2024-09-20 23:22:02 +0200

    add user authorization

--- feature/discussion/server/server_test.go
+++ feature/discussion/server/server_test.go
@@ -1,6 +1,9 @@
 package server_test
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,6 +16,7 @@ func TestServer(t *testing.T) {
 		t.Parallel()
 
 		var repo userRepo
+		srv := testServer(t)
 
 		// Given there is a user Dima.
 
@@ -20,6 +24,9 @@ func TestServer(t *testing.T) {
 		require.NoError(t, err, "getting user Dima")
 
 		// When Dima authorises.
+
+		_, err = http.Post(srv, "text/html", strings.NewReader("authorization"))
+		require.NoError(t, err, "authorizing Dima")
 
 		// Then he can do it.
 
@@ -32,4 +39,16 @@ type userRepo struct{}
 
 func (r *userRepo) GetUser(id string) (*user, error) {
 	return &user{}, nil
+}
+
+func testServer(tb testing.TB) string {
+	tb.Helper()
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	}))
+
+	tb.Cleanup(srv.Close)
+
+	return srv.URL
 }
